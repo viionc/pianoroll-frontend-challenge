1cac6060aaa648be6d2cd3817741ba11ba8a9208
viionc <marek.d.stankiewicz@gmail.com>
2023-10-29 15:57:06 +0100

    fixed selection now good

--- app.js
+++ app.js
@@ -39,54 +39,54 @@ class PianoRollDisplay {
         const svg = clonedRoll.querySelector("svg");
         svg.setAttribute("height", 500);
         clonedRoll.classList.add("main");
-        clonedRoll.onclick = (e) => this.startSelection(e, clonedRoll);
+        clonedRoll.onclick = (e) => this.startSelection(e, svg);
         // add class active (green background) to the clicked roll
         roll.classList.add("active");
         // append the cloned roll to main piano element
 
-        const selection = document.createElement("div");
-        selection.id = "selection";
-        clonedRoll.appendChild(selection);
         mainPianoRoll.appendChild(clonedRoll);
         mainPianoRoll.classList.add("show");
     }
 
-    startSelection(event, element) {
+    startSelection(event, svg) {
         if (this.selectionEndPosition) {
             this.resetSelection();
             return;
         }
-        const clonedRollPosition = element.getBoundingClientRect();
-        const clickedPos = {x: event.clientX - clonedRollPosition.left, y: event.clientY - clonedRollPosition.top};
+        const svgPos = svg.getBoundingClientRect();
+
+        const clickedPos = {x: Math.max(event.clientX - svgPos.left, 0), y: Math.max(event.clientY - svgPos.top, 0)};
         if (!this.selectionStartPosition) {
             this.selectionStartPosition = clickedPos;
         } else {
             this.selectionEndPosition = clickedPos;
-            this.drawSelection();
+            this.drawSelection(svg, svgPos);
         }
     }
 
-    drawSelection() {
-        const selection = document.getElementById("selection");
-        selection.style = `
-            position: absolute;
-            top: ${this.selectionStartPosition.y}px;
-            bottom: ${this.selectionEndPosition.y}px;
-            left: ${this.selectionStartPosition.x}px;
-            right: ${this.selectionEndPosition.x}px;
-            background-color: #ffe6054b;
-            z-index: 20;
-        `;
+    drawSelection(svg, svgPos) {
+        const selectionRect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+        selectionRect.id = "selection";
+        const minY = Math.min(this.selectionStartPosition.y, this.selectionEndPosition.y);
+        const minX = Math.min(this.selectionStartPosition.x, this.selectionEndPosition.x);
+        const maxY = Math.max(this.selectionStartPosition.y, this.selectionEndPosition.y);
+        const maxX = Math.max(this.selectionStartPosition.x, this.selectionEndPosition.x);
+        const y = minY / svgPos.height;
+        const x = minX / svgPos.width;
+        const height = (maxY - minY) / svgPos.height;
+        const width = (maxX - minX) / svgPos.width;
+        selectionRect.setAttribute("fill", "#ffe6054b");
+        selectionRect.setAttribute("x", x);
+        selectionRect.setAttribute("y", y);
+        selectionRect.setAttribute("height", height);
+        selectionRect.setAttribute("width", width);
+        svg.appendChild(selectionRect);
     }
 
     resetSelection() {
+        document.getElementById("selection").remove();
         this.selectionEndPosition = null;
         this.selectionStartPosition = null;
-        selection.style = `
-          position: absolute;
-          background-color: #ffe6054b;
-          z-index: 20;
-      `;
     }
 
     preparePianoRollCard(rollId) {
